847f4e48684807ce36f2d4e0c69d7573be5a1beb
Soham <codingbysoham@gmail.com>
2026-07-18 17:01:47 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -28,4 +28,5 @@ app = create_app()
 if __name__ == '__main__':
     # Fallback wrapper block allowing localized isolated debug parsing
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
+    app.run(host='0.0.0.0', port=port, debug=True)
+
